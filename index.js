148d132fbeef5985f87687b0adbf990a863e1510
JeanSuw <119563534+JeanSuw@users.noreply.github.com>
2023-02-28 18:04:27 -0800

    Revised the array

--- index.js
+++ index.js
@@ -1,41 +1,65 @@
 // TODO: Include packages needed for this application
-// const fs = require('fs');
-// // TODO: Create an array of questions for user input
-// const questions = [
-//     "What is your project title?",
-//     "What is the description of your project?",
-//     "What is the installation instruction for your project?",
-//     "Usage information",
-//     "contribution guidelines",
-//     "test instructions"
-// ];
+const fs = require('fs');
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        message: 'What is your project title?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'What is the description of your project?',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'What is the installation instruction for your project?',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'Usage information?',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'contribution guidelines?',
+        name: 'contribution',
+    },
+    {
+        type: 'input',
+        message: 'test instructions?',
+        name: 'test',
+    },
+];
 
-// // TODO: Create a function to write README file
-// function writeToFile(fileName, data) {
-//     fs.writeFile(fileName, data, ( err) => 
-//         err ? console.error(err) : console.log('Commit logged!')
-//     );
-// }
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, ( err) => 
+        err ? console.error(err) : console.log('Commit logged!')
+    );
+}
 
 // // TODO: Create a function to initialize app
-// function init() {
-//     var inquirer = require('inquirer');
-//     inquirer
-//         .prompt(questions)
-//     .then((answers) => {
-//         // Use user feedback for... whatever!!
-//         answers = process.argv;
-//         console.log(process.argv);
+function init() {
+    var inquirer = require('inquirer');
+    inquirer
+        .prompt(questions)
+    .then((answers) => {
+        // Use user feedback for... whatever!!
+        //answers = process.argv;
+        //console.log(process.argv);
 
-//     })
-//     .catch((error) => {
-//         if (error.isTtyError) {
-//             console.log("Prompt couldn't be rendered in the current environment");
-//         } else {
-//             console.log("How did you get here?");
-//         }
-//     });
-// }
+    })
+    .catch((error) => {
+        if (error.isTtyError) {
+            console.log("Prompt couldn't be rendered in the current environment");
+        } else {
+            console.log("How did you get here?");
+        }
+    });
+}
 
 // //function testArgv(){
 //     // const userInputs = process.argv;
@@ -51,6 +75,6 @@
 
 // //}
 
-// // Function call to initialize app
-// init();
+// Function call to initialize app
+init();
 console.log("Hello");
